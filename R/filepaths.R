f851e828bc590cbac0ae83b813fe9ba161287733
JosephAhrenholtz <83989469+JosephAhrenholtz@users.noreply.github.com>
2024-08-21 08:19:27 -0700

    update readme and filepaths

--- R/filepaths.R
+++ R/filepaths.R
@@ -10,8 +10,29 @@
 #' @examples
 #' filepaths(year = 2024)
 #'
+#' @note
+#' Year 2023 filepaths are used to process 2023 OM education data at 2020 geographies, which is required
+#' for implementation of 3-year rolling education averages.
+#'
 #' @export
 filepaths <- function(year = current_year) {
+
+
+if(year >= 2023){
+  # census
+  assign("census_variables", c(2020, 'census20_variables.csv'), parent.frame())
+  assign("acs_variables", c(2019, 'acs19_variables.csv'), parent.frame())
+  assign("tract_center", c(2020, "CenPop2020_Mean_TR06.txt"), parent.frame())
+  assign("block_group_center", c(2020, "CenPop2020_Mean_BG06.txt"), parent.frame())
+  assign("bg_area", c(2020, "bg_area2020.txt"), parent.frame())
+  assign("tract_area", c(2020, "tract_area2020.txt"), parent.frame())
+  # Education
+  assign("ED_english_math", c(2019, "sb_ca2019_1_csv_v4.txt"), parent.frame())
+  assign("ED_cohort", c(2021, "acgr21.txt"), parent.frame())
+  assign("pubschls", c(2022, "pubschls22.txt"), parent.frame())
+  assign("frpm", c(2022, "frpm2122.xlsx"), parent.frame())
+
+}
 
 
 if(year >= 2024){
@@ -43,7 +64,6 @@ if(year >= 2024){
   assign("county_cog_xwalk", c(2019, "county-cog-xwalk.csv"), parent.frame())
 
   # neighborhood change - get updated code from Matt that includes baseline_race0021.  He sent file but not updated code.
-  #assign("nc_scores", c("R/neighborhood_change/2024/Results/Neighborhood Change Layer/neighborhood_change_layer_2024.csv"), parent.frame())
   assign("nc_scores", c("R/neighborhood_change/2024/Results/Neighborhood Change Layer/neighborhood_change_layer_04042024.csv"), parent.frame())
 
   # supplementary data
@@ -65,6 +85,9 @@ if(year >= 2025){
 
   # region
   assign("USDA_excluded", c('data-raw/shapefiles/2023/SFH_MFH_Ineligible20231206/SFH_MFH_IELG2020_CA.shp'), parent.frame())
+
+  # neighborhood change
+  assign("nc_scores", c("R/neighborhood_change/2025/Results/Neighborhood Change Layer/nbrchange_2022end_08152024.csv"), parent.frame())
 }
 
 
